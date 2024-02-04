d9fcbe856994e3d0eefe4d420fc283bfe661919d
Fahmi-XD <fahmixd404@gmail.com>
2024-02-04 21:19:07 +0700

    Update Tiktok

--- controller/tiktok.controller.js
+++ controller/tiktok.controller.js
@@ -1,8 +1,33 @@
+const axios = require('axios');
 const { TiktokDL } = require('@tobyg74/tiktok-api-dl');
+
+function random_number() {
+    const nm = '1234567890';
+    let re = '';
+    for (let i = 0; i < 8; i++) {
+        let rn = Math.floor(Math.random() * 10);
+        let hehe = nm.charAt(rn);
+        re += hehe;
+    }
+    return re;
+}
+
+async function showVideoMedia(url, res, mediaType) {
+    const response = await axios.get(url, { responseType: 'stream' });
+    if (mediaType.includes('video')) {
+        res.setHeader('Content-Type', 'video/mp4');
+    } else if (mediaType.includes('audio')) {
+        res.setHeader('Content-Type', 'audio/mp3');
+    } else {
+        res.setHeader('Content-Type', 'image/jpeg');
+    }
+    response.data.pipe(res);
+}
 
 const tiktok_download = {
     get_tiktok_data: async (req, res) => {
-        const { url } = req.body;
+        let { url } = req.body;
+        console.log(url);
         if (url) {
             let data_tiktok;
             await TiktokDL(url, {
@@ -15,6 +40,16 @@ const tiktok_download = {
             res.status(400).json({
                 status: '400'
             });
+        }
+    },
+
+    get_tiktok_download: async (req, res) => {
+        const { tiktokUrl, tiktokMediaType } = req.query;
+        try {
+            await showVideoMedia(tiktokUrl, res, tiktokMediaType);
+        } catch (error) {
+            console.log('Error' + error);
+            res.status(500).send('Internal Server Error');
         }
     }
 };
--- routes/tiktok.router.js
+++ routes/tiktok.router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const tiktokController = require('../controller/tiktok.controller');
 
 router.post('/', tiktokController.get_tiktok_data);
+router.get('/download', tiktokController.get_tiktok_download);
 
 module.exports = router;
